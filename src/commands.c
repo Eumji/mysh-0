5383e4565fd025a16c2c96bf271cc6bb44c654d0
eumji <jihye97@ajou.ac.kr>
2017-10-06 12:59:06 +0900

    s

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,94 @@
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <unistd.h>
 
+// this guy, jusuk bbae!
 #include "commands.h"
 
-int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
+int validate_cd_argv(int argc, char** argv);
+int validate_pwd_argv(int argc, char** argv);
 
-  // TODO: Fill it!
-  return 0;
+char buf[256], change_buf[256], cur_buf[256];
+int do_cd(int argc, char** argv) {
+	
+	getcwd(buf, sizeof(buf));
+
+	if (!validate_cd_argv(argc, argv))
+		return -1;
+
+	char c =  argv[1][0];
+
+	if(!strcmp(argv[1],"..")){
+		int len = strlen(buf);
+		int whi=0;
+		for(int i=len-1;i>0;i--){
+			if(buf[i]=='/'){whi=i; break;}
+		}
+		
+		strncpy(change_buf,buf,whi);
+		chdir(change_buf);
+		
+	}
+	else if(!strcmp(argv[1], ".")){
+		chdir(buf);	
+	}
+	else if(c=='/'){
+		chdir(argv[1]);
+	}
+	else if(c=='~'){
+		char usrname[256];
+		
+		getlogin_r(usrname, sizeof(usrname));
+		
+		strcpy(buf, "/home/");
+		strcat(buf, usrname);
+
+		if(strlen(argv[1])>1){
+			strcpy(change_buf, argv[1]+1);
+			strcat(buf,change_buf);
+		}
+
+		chdir(buf);
+	}
+	else{
+		strcat(buf, argv[1]);
+
+		chdir(buf);
+	}
+
+
+	return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+	if (!validate_pwd_argv(argc, argv))
+		return -1;
 
-  // TODO: Fill it!
+	getcwd(buf, sizeof(buf));
+	printf("%s\n", buf);
+	
 
-  return 0;
+	return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(argc==1) return 0;
+	if(argc>2) return 0;
+	//else if (!(!strcmp(argv[1], "..")||!strcmp(argv[1], ".")||argv[1][0]=='/')) return 0;
+	if(strcmp(argv[0], "cd")!=0) return 0;
+	if(argv[1][0]=='.'){
+		if(strlen(argv[1])>2){
+			if(argv[1][2]=='.') return 0;
+		}
+	}
+	return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(argc>1) return 0;
+	if(strcmp(argv[0],"pwd")!=0) return 0;
+
+	return 1;
 }
+
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,48 @@
 #include "utils.h"
+#include <string.h>
+#include<malloc.h>
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int len = strlen(command);
+	char ss[100][100]={0};
+	int wordnum=-1;
+	int isblank=1;
+	int wwhi=0;
+	//argv's (wordnum,wwhi)
+	
+	for(int i=0;i<len;i++){
+		if(command[i]==' ' || command[i] == '\t' || command[i] == '\n'){
+			if(isblank==0)
+				isblank=1;
+		}
+		else{
+			if(isblank==1){
+				wordnum++;
+				wwhi=0;
+				ss[wordnum][wwhi++] = command[i];
+				isblank=0;
+			}
+			else{
+				ss[wordnum][wwhi++] = command[i];
+			}
+		}
+	}
+
+	if(wordnum == -1){
+		*argc = 1;
+		*argv = (char **)malloc(sizeof(char*));
+		(*argv)[0] = (char*)malloc(sizeof(char));
+		return;
+	}
+
+	*argv = (char **)malloc(sizeof(char*) * (wordnum+1));
+	for(int i=0;i<=wordnum;i++){
+		(*argv)[i]=(char*)malloc(sizeof(char)*(strlen(ss[i])+1));
+		strcpy((*argv)[i], ss[i]);
+	}
+
+	*argc=wordnum+1;
 }
+
+
