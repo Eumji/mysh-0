37f703594699cdda6b42de08b6ffab001f51cdea
eumji <jihye97@ajou.ac.kr>
2017-10-07 13:31:33 +0900

    171007 complete commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,92 +1,90 @@
+#include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <unistd.h>
-
-// this guy, jusuk bbae!
+#include <stdlib.h>
+#include <pwd.h>
 #include "commands.h"
 
 int validate_cd_argv(int argc, char** argv);
 int validate_pwd_argv(int argc, char** argv);
 
 char buf[256], change_buf[256], cur_buf[256];
+char temp[256]; 
+char usr_name[256];
+
 int do_cd(int argc, char** argv) {
-	
+
 	getcwd(buf, sizeof(buf));
 
 	if (!validate_cd_argv(argc, argv))
-		return -1;
+		return 1;
 
-	char c =  argv[1][0];
-
-	if(!strcmp(argv[1],"..")){
-		int len = strlen(buf);
-		int whi=0;
-		for(int i=len-1;i>0;i--){
-			if(buf[i]=='/'){whi=i; break;}
-		}
-		
-		strncpy(change_buf,buf,whi);
-		chdir(change_buf);
-		
-	}
-	else if(!strcmp(argv[1], ".")){
-		chdir(buf);	
-	}
-	else if(c=='/'){
-		chdir(argv[1]);
-	}
-	else if(c=='~'){
-		char usrname[256];
-		
-		getlogin_r(usrname, sizeof(usrname));
-		
-		strcpy(buf, "/home/");
-		strcat(buf, usrname);
-
-		if(strlen(argv[1])>1){
-			strcpy(change_buf, argv[1]+1);
-			strcat(buf,change_buf);
-		}
-
-		chdir(buf);
-	}
-	else{
-		strcat(buf, argv[1]);
-
-		chdir(buf);
-	}
-
+	if(argv[1][0] != '~') chdir(argv[1]);
+	else chdir(temp);
 
 	return 0;
 }
 
 int do_pwd(int argc, char** argv) {
 	if (!validate_pwd_argv(argc, argv))
-		return -1;
+		return 1;
 
 	getcwd(buf, sizeof(buf));
 	printf("%s\n", buf);
-	
 
 	return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-	if(argc==1) return 0;
-	if(argc>2) return 0;
-	//else if (!(!strcmp(argv[1], "..")||!strcmp(argv[1], ".")||argv[1][0]=='/')) return 0;
+
+	if(argc == 1) return 0;
+
+	if(argc > 2) return 0;
+
 	if(strcmp(argv[0], "cd")!=0) return 0;
-	if(argv[1][0]=='.'){
-		if(strlen(argv[1])>2){
-			if(argv[1][2]=='.') return 0;
-		}
+
+	getcwd(cur_buf, sizeof(cur_buf));
+
+	int flag = 0;
+
+	if(argv[1][0] == '~'){
+
+		memset(temp, 0, sizeof(temp));
+
+		// get user id
+		struct passwd *pw;
+
+		uid_t uid;
+
+		uid = geteuid();
+
+		pw = getpwuid(uid);
+		
+
+
+		strcpy(temp, "/home/");
+
+		strcat(temp,pw->pw_name);
+
+		strcat(temp, argv[1]+1); 
+
+		flag = chdir(temp);
 	}
+
+	else flag = chdir(argv[1]);
+
+	chdir(cur_buf);
+
+	if(flag == -1) return 0;
+
 	return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
 	if(argc>1) return 0;
+
 	if(strcmp(argv[0],"pwd")!=0) return 0;
 
 	return 1;
