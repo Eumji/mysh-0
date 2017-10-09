8d76c2cc4adb499113088f7b19f76454f84a386e
eumji <jihye97@ajou.ac.kr>
2017-10-09 15:15:56 +0900

    add comments

--- src/commands.c
+++ src/commands.c
@@ -16,67 +16,69 @@ char usr_name[256];
 
 int do_cd(int argc, char** argv) {
 
-	getcwd(buf, sizeof(buf));
+	getcwd(buf, sizeof(buf)); // get current directory
 
-	if (!validate_cd_argv(argc, argv))
+	if (!validate_cd_argv(argc, argv)) // validate
 		return 1;
 
-	if(argv[1][0] != '~') chdir(argv[1]);
+	// move directory
+	
+	if(argv[1][0] != '~') chdir(argv[1]); // i made target directory in validate function. and i use it
+	
 	else chdir(temp);
 
 	return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-	if (!validate_pwd_argv(argc, argv))
+	if (!validate_pwd_argv(argc, argv)) // validate
 		return 1;
 
-	getcwd(buf, sizeof(buf));
+	getcwd(buf, sizeof(buf)); // get current directory
 	
-	fprintf(stderr, "%s\n", buf);
+	fprintf(stderr, "%s\n", buf); // print using stderr stream
 	
 	return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
 
-	if(argc == 1) return 0;
+	if(argc == 1) return 0; // cd argc must be 2
 
-	if(argc > 2) return 0;
+	if(argc > 2) return 0; // cd argc must be 2
 
-	if(strcmp(argv[0], "cd")!=0) return 0;
+	if(strcmp(argv[0], "cd")!=0) return 0; // command check
 
-	getcwd(cur_buf, sizeof(cur_buf));
+	getcwd(cur_buf, sizeof(cur_buf)); // save current directory
 
-	int flag = 0;
+	int flag = 0; // -1 : error
 
-	if(argv[1][0] == '~'){
+	if(argv[1][0] == '~'){ // '~' need user name
 
 		memset(temp, 0, sizeof(temp));
 
-		// get user id
-		struct passwd *pw;
+		struct passwd *pw; // for get user name
 
 		uid_t uid;
 
 		uid = geteuid();
 
 		pw = getpwuid(uid);
-		
 
-
+		// make target directory
 		strcpy(temp, "/home/");
 
 		strcat(temp,pw->pw_name);
 
 		strcat(temp, argv[1]+1); 
 
-		flag = chdir(temp);
+		flag = chdir(temp); // check if it work rightly
 	}
 
-	else flag = chdir(argv[1]);
+	// except '~', we can use argv[1] to directory
+	else flag = chdir(argv[1]); // check if it work rightly.
 
-	chdir(cur_buf);
+	chdir(cur_buf); // move to original directory
 
 	if(flag == -1) return 0;
 
@@ -84,9 +86,9 @@ int validate_cd_argv(int argc, char** argv) {
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-	if(argc>1) return 0;
+	if(argc>1) return 0; // pwd argc must be 1
 
-	if(strcmp(argv[0],"pwd")!=0) return 0;
+	if(strcmp(argv[0],"pwd")!=0) return 0; // command check
 
 	return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -5,39 +5,44 @@
 void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
 	int len = strlen(command);
-	char ss[100][100]={0};
-	int wordnum=-1;
-	int isblank=1;
-	int wwhi=0;
-	//argv's (wordnum,wwhi)
+	char ss[100][100]={0}; // to save datas(words) temporary
+	int wordnum=-1; // number of words
+	int isblank=1; // 0 : not blank, 1 : blank
+	int wwhi=0; // it use for add characters(index)
 	
+
 	for(int i=0;i<len;i++){
+		// if next character is blank
 		if(command[i]==' ' || command[i] == '\t' || command[i] == '\n'){
 			if(isblank==0)
 				isblank=1;
 		}
+
+		// not blank
 		else{
-			if(isblank==1){
-				wordnum++;
-				wwhi=0;
-				ss[wordnum][wwhi++] = command[i];
-				isblank=0;
+			if(isblank==1){ // if previous character is blank
+				wordnum++; // increse number of word
+				wwhi=0; // initialize index
+				ss[wordnum][wwhi++] = command[i]; // add first character in next words
+				isblank=0; // check not blank
 			}
 			else{
-				ss[wordnum][wwhi++] = command[i];
+				ss[wordnum][wwhi++] = command[i]; // add next character in current words
 			}
 		}
 	}
 
-	if(wordnum == -1){
-		*argc = 1;
+	if(wordnum == -1){ // if there is no words
+		*argc = 1; 
 		*argv = (char **)malloc(sizeof(char*));
 		(*argv)[0] = (char*)malloc(sizeof(char));
 		return;
 	}
 
+	// use malloc for words
 	*argv = (char **)malloc(sizeof(char*) * (wordnum+1));
 	for(int i=0;i<=wordnum;i++){
+		// use malloc and copy words
 		(*argv)[i]=(char*)malloc(sizeof(char)*(strlen(ss[i])+1));
 		strcpy((*argv)[i], ss[i]);
 	}
